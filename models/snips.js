73c36b4d43a0223adaf1d743817c91e015ea4c4e
JustinOleskii <joashm14@gmail.com>
2021-10-06 07:19:46 +0530

    start creating backend and rewrite Form

---
+++ models/snips.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const snipSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: false
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Snips', snipSchema)
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,19 +4,36 @@ import { Input } from '@chakra-ui/input'
 import { Text } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/button'
 import { useColorModeValue } from '@chakra-ui/color-mode'
+import {
+    FormControl,
+    FormLabel,
+} from "@chakra-ui/react"
 
 function Form() {
     const [resize, setResize] = React.useState('none');
+    const [title, setTitle] = React.useState('');
+    const [content, setContent] = React.useState('');
 
     const buttonColors = useColorModeValue('green', 'teal');
 
+    function onSubmit(evt) {
+        evt.preventDefault()
+        console.log(`Title: ${title}\nContent: ${content}`)
+    }
+
     return (
         <>
-            <Text mt="30" size="sm">Title</Text>
-            <Input placeholder="Enter snip title here" mb="10"/>
-            <Text size="sm">Content</Text>
-            <Textarea placeholder="Input your code here" size="md" resize={resize} minHeight="400px"/>
-            <Button mt="5" mb="5" variant="solid" colorScheme={buttonColors} loadingText="Submitting">Submit</Button>
+            <form onSubmit={onSubmit}>
+                <FormControl isRequired mt={2}>
+                    <FormLabel>Title</FormLabel>
+                    <Input placeholder="Enter title here" size="md" onChange={event => setTitle(event.currentTarget.value)} value={title}/>
+                </FormControl>
+                <FormControl isRequired mt={6}>
+                    <FormLabel>Snippet</FormLabel>
+                    <Textarea placeholder="Input snip here" size="md" resize={resize} minHeight="400px" onChange={event => setContent(event.currentTarget.value)} value={content} />
+                </FormControl>
+                <Button mt={5} mb={5} variant="solid" colorScheme={buttonColors} type="submit">Submit</Button>
+            </form>
         </>
     )
 }
